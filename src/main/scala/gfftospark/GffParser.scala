84237c422865ab37aa38bcfc6ffbe0785c0d69b2
Werner de Groot <github@wernerdegroot.nl>
2017-06-23 12:28:02 +0200

    Parser (using Scala parser combinators) for GFF-file

--- src/main/scala/gfftospark/GffParser.scala
+++ src/main/scala/gfftospark/GffParser.scala
@@ -1,59 +1,134 @@
 package gfftospark
 
 import scala.util.Try
+import scala.util.parsing.combinator.JavaTokenParsers
 
 /**
   * Functions for parsing lines of a GFF file
   */
-object GffParser {
-  /**
-    * Parses a single line of a GFF file
-    *
-    * @param line
-    */
-  def parseLine(line: String): GffLine = {
-    val fields = line.split("\t")
-    require(fields.length == 9)
+object GffParser extends JavaTokenParsers {
 
-    val attributesFieldColumnIndex = 8
-    val attributesField = fields(attributesFieldColumnIndex)
+  val attributeKeyValueSeparator = ";"
 
-    // Try to parse a list of attributes (key value pairs) or if that fails, a single string
-    // NOTE: lines with genes and transcripts seem to have a single string with the gene ID / transcript ID
-    val attributes = Try(parseAttributesMap(attributesField)).map(Right(_))
-      .getOrElse(Left(attributesField))
+  def notDoubleQuoteRegex = "[^\"]*".r
 
-    GffLine(
-      fields(0),
-      fields(1),
-      fields(2),
-      fields(3).toLong,
-      fields(4).toLong,
-      0L, // TODO fields(5).toDouble,
-      Strand.fromString(fields(6)),
-      0L, // TODO fields(7).toLong,
-      attributes
-    )
+  def oneOrMoreNonWhitespaceRegex = "[^\\s]+".r
+
+  def orPeriod[A](p: Parser[A]): Parser[Option[A]] = {
+
+    def periodAsNone: Parser[Option[Nothing]] = literal(".") map (_ => None)
+
+    def wrapInSome[A](p: Parser[A]): Parser[Option[A]] = p map (a => Some(a))
+
+    wrapInSome(p) | periodAsNone
   }
 
-  def parseAttributesMap(a: String): Map[String, String] =
-    a.split(";")
-      .map(_.trim) // Remove spaces after the ;
-      .filter(_.nonEmpty) // Each attribute ends with a ;, so we remove the last empty one
-      .map(parseKeyValuePair)
-      .toMap
+  def long: Parser[Long] = wholeNumber ^^ (_.toLong)
 
-  def parseKeyValuePair(kvp: String): (String, String) = {
-    kvp.split(" ").toSeq match {
-      case key +: tail =>
-        val valueWithQuotes = tail.mkString(" ")
-        //        println(s"Parsing key: ${key}, value: ${valueWithQuotes}")
-        val value = valueWithQuotes.substring(1, valueWithQuotes.length - 1)
+  def double: Parser[Double] = floatingPointNumber ^^ (_.toDouble)
 
-        (key, value)
-      case _ =>
-        throw new IllegalArgumentException("Ongeldig attribute: kvp")
+  def seqname: Parser[String] =
+    oneOrMoreNonWhitespaceRegex withFailureMessage "Expected a `seqname`"
+
+  def source: Parser[String] =
+    oneOrMoreNonWhitespaceRegex withFailureMessage "Expected a `source`"
+
+  def feature: Parser[String] =
+    oneOrMoreNonWhitespaceRegex withFailureMessage "Expected a `feature`"
+
+  def start: Parser[Long] =
+    long withFailureMessage "Expected a `start`"
+
+  def end: Parser[Long] =
+    long withFailureMessage "Expected an `end`"
+
+  def score: Parser[Option[Double]] =
+    orPeriod(double) withFailureMessage "Expected a `score`"
+
+  def strand: Parser[Option[Strand]] = {
+    val plusParser = "+" ^^ (_ => Forward)
+    val minusParser = "-" ^^ (_ => Reverse)
+    orPeriod(plusParser | minusParser) withFailureMessage "Expected a `strand`"
+  }
+
+  def frame: Parser[Option[Long]] =
+    orPeriod(("0" | "1" | "2").map(_.toLong)) withFailureMessage "Expected a `frame`"
+
+  def attributeValueWithQuotes: Parser[String] =
+    "\"" ~> notDoubleQuoteRegex <~ "\"" withFailureMessage "Expected an attribute value, surrounded by quotes"
+
+  def attributeValueWithoutQuotes: Parser[String] =
+    """[^";\s]+""".r withFailureMessage "Expected an attribute value"
+
+  def attributeValue = attributeValueWithQuotes | attributeValueWithoutQuotes
+
+  def attributeKey: Parser[String] =
+    """[^";\s=]+""".r withFailureMessage "Expected an attribute key"
+
+  def attributeKeyValue: Parser[(String, String)] =
+    (attributeKey <~ opt("=")) ~ attributeValue map {
+      case key ~ value => (key, value)
     }
+
+  def attributeKeyValues: Parser[Map[String, String]] =
+    rep1sep(attributeKeyValue, attributeKeyValueSeparator) <~ opt(
+      attributeKeyValueSeparator) map (_.toMap)
+
+  def attributes: Parser[Either[String, Map[String, String]]] = {
+    def asLeft[A](p: Parser[A]) = p map (Left(_))
+    def asRight[A](p: Parser[A]) = p map (Right(_))
+
+    asRight(phrase(attributeKeyValues)) | asLeft(".*".r)
+  }
+
+  def line =
+    seqname ~
+      source ~
+      feature ~
+      start ~
+      end ~
+      score ~
+      strand ~
+      frame ~
+      attributes map {
+      case seqname ~ source ~ feature ~ start ~ end ~ score ~ strand ~ frame ~ attributes =>
+        GffLine(seqname,
+                source,
+                feature,
+                start,
+                end,
+                score,
+                strand,
+                frame,
+                attributes)
+    }
+
+  def parseLine(s: String): GffLine =
+    parseAll(line, s).getOrElse(throw new IllegalArgumentException)
+}
+
+object App {
+  def main(args: Array[String]): Unit = {
+
+    val toParse =
+      """
+        |3 transcribed_unprocessed_pseudogene  gene        11869 14409 . - . henk devries; gene_id "ENSG00000223972"; henk de.vries; Note "Clone Y74C9A; Genbank AC024206"; gene_name "DDX11L1"; gene_source "havana"; gene_biotype "transcribed_unprocessed_pseudogene";
+        |1 processed_transcript                transcript  11869 14409 . + . roborovski a 1
+        |Chr3   giemsa heterochromatin  4500000 6000000 . . .   Band 3q12.1
+        |Chr3 giemsa heterochromatin 4500000 6000000 . . . Band 3q12.1 ; Note "Marfan's syndrome"
+        |Chr1        assembly Link   10922906 11177731 . . . Target Sequence:LINK_H06O01 1 254826
+        |LINK_H06O01 assembly Cosmid 32386    64122    . . . Target Sequence:F49B2       6 31742
+        |X	Ensembl	Repeat	2419108	2419128	42	.	.	hid=trf; hstart=1; hend=21
+        |X	Ensembl	Repeat	2419108	2419410	2502	-	.	hid=AluSx; hstart=1; hend=303
+        |X	Ensembl	Repeat	2419108	2419128	0	.	.	hid=dust; hstart=2419108; hend=2419128
+        |X	Ensembl	Pred.trans.	2416676	2418760	450.19	-	2	genscan=GENSCAN00000019335
+        |X	Ensembl	Variation	2413425	2413425	.	+	.
+        |X	Ensembl	Variation	2413805	2413805	.	+	.
+        """.stripMargin
+
+    val lines = toParse.split("\n") map (_.trim) filter (_.nonEmpty)
+
+    lines map GffParser.parseLine foreach println
   }
 }
 
@@ -63,8 +138,9 @@ case class GffLine(
                     feature: String,
                     start: Long,
                     stop: Long,
-                    score: Double,
-                    strand: Strand,
-                    frame: Long,
+                    score: Option[Double],
+                    strand: Option[Strand],
+                    frame: Option[Long],
                     attributes: Either[String, Map[String, String]] // Either a single string or list of attributes
                   ) extends Serializable
+
