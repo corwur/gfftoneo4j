4c80f383521e3ebfe13b3a2149988fb36e0e18a9
Steven Vroonland <steven.vroonland@ordina.nl>
2017-09-21 13:40:42 +0200

    Remove Spark
    Parallelization via Spark does not offer any benefit with the current algorithm, since we need the whole file to build the tree structure and group by sequence anyway

--- src/main/scala/corwur/Application.scala
+++ src/main/scala/corwur/Application.scala
@@ -4,10 +4,9 @@ import corwur.CommandLineParser.CommandLineArgs
 import corwur.genereader.{DnaSequence, GeneReaders}
 import corwur.gffparser.{GffLine, GffLineOrHeader, GffParser}
 import corwur.neo4j.GenesToNeo4j
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
 import org.neo4j.graphdb.RelationshipType
 
+import scala.io.Source
 import scala.util.{Failure, Success}
 
 /**
@@ -16,13 +15,6 @@ import scala.util.{Failure, Success}
   * Reads and parses a GFF file, construct Genes and insert them into a Neo4j database
   */
 object Application {
-
-  @transient lazy val conf: SparkConf = new SparkConf()
-    .setMaster("local")
-    .setAppName("gfftoneo4j")
-    .set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
-  @transient lazy val sc: SparkContext = new SparkContext(conf)
-
   /** Main function */
   def main(args: Array[String]): Unit =
     CommandLineParser.parser
@@ -30,43 +22,38 @@ object Application {
       .foreach(importGffFile)
 
   private def importGffFile(params: CommandLineArgs) = {
-    try {
-      // Read lines
-      val lines: RDD[String] = sc.textFile(params.file)
-      val reader = GeneReaders.geneReadersById.get(params.format).getOrElse(throw new IllegalArgumentException("No such reader found"))
-      val dbUrl = params.neo4jUrl
+    // Read lines
+    val lines: Iterator[String] = Source.fromFile(params.file).getLines()
+    val reader = GeneReaders.geneReadersById.get(params.format).getOrElse(throw new IllegalArgumentException("No such reader found"))
+    val dbUrl = params.neo4jUrl
 
-      // Parse lines into a meaningful data structure (GffLine)
-      val gffLines: RDD[GffLineOrHeader] = lines
-        .map { l =>
-            GffParser.parseLineOrHeader(l)
+    // Parse lines into a meaningful data structure (GffLine)
+    val gffLines: Iterator[GffLineOrHeader] = lines
+      .map { l =>
+        GffParser.parseLineOrHeader(l)
           .fold(msg => Failure(new IllegalArgumentException(s"Parse error in line '${l}': $msg")), Success.apply)
-            .get
-        }
-
-      // Discard headers and group by sequence
-      val linesPerSequence = gffLines
-        .flatMap {
-          case g@GffLine(_, _, _, _, _, _, _, _, _) => Seq(g)
-          case _ => Seq.empty
-        }
-        .groupBy { case GffLine(seqname, _, _, _, _, _, _, _, _) => seqname }
-
-      val sequences = linesPerSequence.map { case (sequenceName, lines) =>
-        val genes = reader(lines)
-
-        DnaSequence(sequenceName, genes)
+          .get
       }
 
-      // Insert in database
-      GenesToNeo4j.insertSequences(sequences.collect(), dbUrl)
+    // Discard headers and group by sequence
+    val linesPerSequence = gffLines
+      .flatMap {
+        case g@GffLine(_, _, _, _, _, _, _, _, _) => Seq(g)
+        case _ => Seq.empty
+      }
+      .toSeq
+      .groupBy { case GffLine(seqname, _, _, _, _, _, _, _, _) => seqname }
 
-      val nrGenes = sequences.map(_.genes.size).collect().sum
-      println("Number of genes processed: " + nrGenes)
+    val sequences = linesPerSequence.map { case (sequenceName, lines) =>
+      val genes = reader(lines)
+      DnaSequence(sequenceName, genes)
     }
-    finally {
-      sc.stop()
-    }
+
+    // Insert in database
+    GenesToNeo4j.insertSequences(sequences.toSeq, dbUrl)
+
+    val nrGenes = sequences.map(_.genes.size).sum
+    println("Number of genes processed: " + nrGenes)
   }
 }
 
